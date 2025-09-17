3d3d26939b1a242538aa42ebd725f1ae3c07a40b
akhilp-boop <akhil.p@orchidsintl.edu.in>
2025-09-17 17:23:16 +0530

    Update Activity_1.2.ino

--- Grade7_2025-26/Chapter 1/Activity_1.2/Activity_1.2.ino
+++ Grade7_2025-26/Chapter 1/Activity_1.2/Activity_1.2.ino
@@ -1,45 +1,53 @@
 // Define input pins for Motor 1
 const int input11 = 7;
 const int input12 = 8;
-// Define input pins for Motor 2
+// Define input pins for Motor 1
 const int input21 = 9;
 const int input22 = 10;
-// Define enable (PWM) pins for Motor 1 and Motor 2
-const int enable1 = 3;
-const int enable2 = 5;
 
 void setup()
 {
-  // Set motor input pins as output
+  // Set pins 7, 8, 9, and 10 as output pins
   pinMode(input11, OUTPUT);
   pinMode(input12, OUTPUT);
   pinMode(input21, OUTPUT);
   pinMode(input22, OUTPUT);
-  // Set enable pins as output
-  pinMode(enable1, OUTPUT);
-  pinMode(enable2, OUTPUT);
 }
+
 void loop()
 {
-  // Set PWM signal to ~75% (191 out of 255) for both motors
-  // Motor 1 will run at ~75% speed
-  analogWrite(enable1, 191);
-  // Motor 2 will run at ~75% speed
-  analogWrite(enable2, 191);
-  // Set Motor 1 to move forward (clockwise)
-  // input11 = HIGH, input12 = LOW
+  // To move Motor 1 clockwise, set input11 = HIGH and input12 = LOW
   digitalWrite(input11, HIGH);
   digitalWrite(input12, LOW);
-  // Set Motor 2 to move forward (clockwise)
-  // input21 = HIGH, input22 = LOW
+  // To move Motor 2 clockwise, set input21 = HIGH and input22 = LOW
   digitalWrite(input21, HIGH);
   digitalWrite(input22, LOW);
-  // Run both motors forward for 3 seconds
+  // Run both motors clockwise for 3 seconds
   delay(3000);
-  // Stop both motors by setting inputs to LOW
+
+  // Stop both motors by setting all inputs LOW
   digitalWrite(input11, LOW);
   digitalWrite(input12, LOW);
   digitalWrite(input21, LOW);
   digitalWrite(input22, LOW);
+  // Wait for 2 second while motors are stopped
+  delay(2000);
+
+  // To move Motor 1 move anti-clockwise, input11 = LOW, input12 = HIGH
+  digitalWrite(input11, LOW);
+  digitalWrite(input12, HIGH);
+  // To move Motor 2 move anti-clockwise, input21 = LOW, input22 = HIGH
+  digitalWrite(input21, LOW);
+  digitalWrite(input22, HIGH);
+  // Run both motors anti-clockwise for 3 seconds
+  delay(3000);
+  
+  // Stop both motors by setting all inputs LOW
+  digitalWrite(input11, LOW);
+  digitalWrite(input12, LOW);
+  digitalWrite(input21, LOW);
+  digitalWrite(input22, LOW);
+  // Wait for 2 second while motors are stopped
+  delay(2000);
 }
 
